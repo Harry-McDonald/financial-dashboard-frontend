4a33ac471ab86860c6c70d244f088573dc5f16af
Harry McDonald <s4430327@student.uq.edu.au>
2024-02-19 20:50:54 +1000

    New build files.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// export const DOMAIN_NAME = "http://13.211.163.215/";
-export const DOMAIN_NAME = "http://localhost:3000/";
+export const DOMAIN_NAME = "http://13.211.163.215/";
+// export const DOMAIN_NAME = "http://localhost:3000/";
 // export const BACKEND_API_URL = "http://localhost:5000/";
 export const BACKEND_API_URL = "http://13.211.163.215:5000/";
 export const TOKEN_BALANCE_TABLENAME = "crypto_token_balances";
