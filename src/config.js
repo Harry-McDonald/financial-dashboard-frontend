1f064a5a257c94aef00ee83a094d5a74c722db1b
Harry McDonald <s4430327@student.uq.edu.au>
2024-02-25 11:25:10 +1000

    Build

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-// export const DOMAIN_NAME = "http:/54.206.238.232/"; // Production
-// export const BACKEND_API_URL = "http://54.206.238.232/api/";
-export const DOMAIN_NAME = "http://localhost:3000/"; // developmeent
-export const BACKEND_API_URL = "http://localhost:5000/";
+export const DOMAIN_NAME = "http:/54.206.238.232/"; // Production
+export const BACKEND_API_URL = "http://54.206.238.232/api/"; // Production
+// export const DOMAIN_NAME = "http://localhost:3000/"; // developmeent
+// export const BACKEND_API_URL = "http://localhost:5000/"; // Development
 export const TOKEN_BALANCE_TABLENAME = "crypto_token_balances";
 export const FINANCIAL_HISTORICAL_DATA_TABLENAME = "historical_financial_data";
